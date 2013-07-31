a46fc3a1b185935b1516b54dcebe1c0e6520a6f8
Mika Sjoman <mikasjoman@gmail.com>
2013-07-31 15:14:51 +0000

    Added index.html

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fileWithText = new Buffer();
+fileWithText.write(fs.readFileSync('index.html', 'w'), 'utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World! Mikas');
+  response.send(fileWithText.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
