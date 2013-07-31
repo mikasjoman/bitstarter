7990a5d0ac1fbbac18b94adc005efd00cc0d8730
Mika Sjoman <mikasjoman@gmail.com>
2013-07-31 15:20:30 +0000

    Added index.html

--- web.js
+++ web.js
@@ -2,8 +2,19 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var fileWithText = new Buffer();
-fileWithText.write(fs.readFileSync('index.html', 'w'), 'utf-8');
+var fs = require('fs');
+
+var fileWithText = new BufferedReader ("index.html", { encoding: "utf8" })
+    .on ("error", function (error){
+        console.log ("error: " + error);
+    })
+    .on ("line", function (line){
+        console.log ("line: " + line);
+    })
+    .on ("end", function (){
+        console.log ("EOF");
+    })
+    .read ();
 
 app.get('/', function(request, response) {
   response.send(fileWithText.toString('utf-8'));
